b9dc433d55c6b454f5886c28aa8ca974b424a5f9
Đăng Quang <tdquang1196@hotmail.com>
2018-11-13 21:38:46 +0700

    do task controller and datatype controller

---
+++ CodeExam/CodeExam/Areas/Admin/Controllers/DataTypeController.cs
@@ -0,0 +1,67 @@
+﻿using CodeExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeExam.Areas.Admin.Controllers
+{
+    public class DataTypeController : Controller
+    {
+        CodeWarDbContext db = new CodeWarDbContext();
+        // GET: Admin/DataType
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public JsonResult GetAll()
+        {
+            return Json(db.DataTypes, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetDataTypeById(int id)
+        {
+            return Json(db.DataTypes.FirstOrDefault(f=>f.DataTypeId == id), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult CreateDataType(DataType data)
+        {
+            db.DataType.Add(data);
+            if (db.SaveChanges() == 1)
+            {
+                return Json(0, JsonRequestBehavior.AllowGet);
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(DataType data)
+        {
+            var item = db.DataTypes.FirstOrDefault(f => f.DataTypeId == data.DataTypeId);
+            if (item != null)
+            {
+                item.DataTypeName = data.DataTypeName;
+                item.DisplayName = data.DisplayName;
+                if (db.SaveChanges() == 1)
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int id)
+        {
+            var itemToDel = db.DataTypes.FirstOrDefault(f => f.DataTypeId == id);
+            if (itemToDel != null)
+            {
+                itemToDel.DataTypeStatus = Constant.Status.Deactive;
+                if (db.SaveChanges() == 1)
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- CodeExam/CodeExam/Areas/Admin/Controllers/TaskController.cs
+++ CodeExam/CodeExam/Areas/Admin/Controllers/TaskController.cs
@@ -3,16 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CodeExam.Models;
 
 namespace CodeExam.Areas.Admin.Controllers
 
 {
     public class TaskController : Controller
     {
+        CodeWarDbContext db = new CodeWarDbContext();
         // GET: Areas/Task
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public JsonResult GetAll()
+        {
+            return Json(db.Tasks, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult CreateTask(Task task)
+        {
+            db.Tasks.Add(task);
+            if (db.SaveChanges() == 1)
+            {
+                return Json(0, JsonRequestBehavior.AllowGet);
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(Task task)
+        {
+            var item = db.Tasks.FirstOrDefault(f => f.TaskId == task.TaskId);
+            if (item != null)
+            {
+                item.Input = task.Input;
+                item.OutputType = task.OutputType;
+                item.Point = task.Point;
+                item.TaskDescription = task.TaskDescription;
+                item.TaskLevel = task.TaskLevel;
+                item.TaskName = task.TaskName;
+                item.TestCaseId = task.TestCaseId;
+                if (db.SaveChanges() == 1)
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetTaskById(int id)
+        {
+            return Json(db.Tasks.FirstOrDefault(f => f.TaskId == id), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int id)
+        {
+            var itemToDel = db.Tasks.FirstOrDefault(f => f.TaskId == id);
+            if (itemToDel != null)
+            {
+                itemToDel.TaskStatus = Constant.Status.Deactive;
+                if (db.SaveChanges() == 1)
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(1, JsonRequestBehavior.AllowGet);
         }
     }
 }
