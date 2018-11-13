f24636e63963d88e03bc0e0d865467426757963d
Đăng Quang <tdquang1196@hotmail.com>
2018-11-13 20:28:35 +0700

    add constant

---
+++ CodeExam/CodeExam/Constant/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeExam.Constant
+{
+    public static class Status
+    {
+        public static int Active = 0;
+        public static int Deactive = 1;
+    }
+}
