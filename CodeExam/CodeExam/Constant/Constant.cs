535fcced15bcfb2efb7c61f105ef06d82ad2207f
Đăng Quang <tdquang1196@hotmail.com>
2018-11-13 23:15:32 +0700

    no message

--- CodeExam/CodeExam/Constant/Constant.cs
+++ CodeExam/CodeExam/Constant/Constant.cs
@@ -10,4 +10,10 @@ namespace CodeExam.Constant
         public static int Active = 0;
         public static int Deactive = 1;
     }
+    public static class TaskLevel
+    {
+        public static string Easy = "Easy";
+        public static string Medium = "Medium";
+        public static string Hard = "Hard";
+    }
 }
